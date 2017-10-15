7c4c099157b6ef04a82330220dd4fcba2f78046f
FakeDoorComputing <32259974+FakeDoorComputing@users.noreply.github.com>
2017-10-15 17:17:45 +0100

    Add files via upload

--- js/saveload.js
+++ js/saveload.js
@@ -1,19 +1,55 @@
-function save(){
-var kval=[], ret=[];
-var x = $("form").serializeArray();
-$.each(x, function(i, field){
-  kval[i]=field.value;
-});
-  for(n=0, o=1;n<9;n+=2, o+=2){
-    window.localStorage.setItem(kval[n],kval[o]);
-  }
-  for(n=0;n<9;n+=2){
-    ret[n]=window.localStorage.getItem(kval[n]);
-  };
-  for(m=1, n=0, o=1; m<11; m++, n+=2, o+=2){
-  $("#kv"+m).text(kval[n]+" : "+kval[o]);
+var chocolates={"item":[
+  {"name":"Mars Bar","weight":"51g","kcal":"228","fat":"8.5g","carbohydrate":"35.3g","sugar":"30.5g","salt":"0.21g"},
+  {"name":"Galaxy Honeycomb","weight":"22.8g","Kcal":"125","fat":"7.3g","carbohydrate":"13.2g","sugar":"13.0g","salt":"0.06g"},
+  {"name":"Maltesers","weight":"37g","Kcal":"186","fat":"9.1g","carbohydrate":"22.8g","sugar":"19.1g","salt":"0.17g"}
+]};
+
+var chocNo=0;
+
+var chocString=JSON.stringify(chocolates);
+window.localStorage.setItem("chocData",chocString);
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+
+    $("#mars").click(function(){
+      chocNo=0;
+      displayNV();
+    });
+
+    $("#galaxy").click(function(){
+      chocNo=1;
+      displayNV();
+    });
+
+    $("#maltesers").click(function(){
+      chocNo=2;
+      displayNV();
+    });
+}
+
+function displayNV(){
+  var name=[],weight=[],cal=[],fat=[],carb=[],sugar=[],salt=[];
+  var getChoc=window.localStorage.getItem("chocData");
+  var chocParse=JSON.parse(getChoc);
+  for(i=0;i<chocParse.item.length;i++){
+    var counter=chocParse.item[i];
+    name[i]=counter.name;
+    weight[i]=counter.weight;
+    cal[i]=counter.kcal;
+    fat[i]=counter.fat;
+    carb[i]=counter.carbohydrate;
+    sugar[i]=counter.sugar;
+    salt[i]=counter.salt;
   };
 
-  $("#login").fadeOut("slow",function(){$(".savedData").fadeIn();});
-  window.localStorage.clear();
-}
+//  window.localStorage.clear();
+
+$("#choc").text(name[chocNo]);
+$("#chocWeight").text(weight[chocNo]);
+$("#chocCalFat").text("Each bar contains "+cal[chocNo]+" calories and "+fat[chocNo]+" of fat,");
+$("#chocCarbSug").text(carb[chocNo]+" of carbohydrates (of which "+sugar[chocNo]+" is sugar),");
+$("#chocSalt").text("and "+salt[chocNo]+" of salt.");
+
+$("#displayValues").fadeIn();
+};
